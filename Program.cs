443879064e3c451c13b826c2b4b4d4e20abe7284
ljlirir <2359806986@qq.com>
2024-11-21 16:22:14 +0800

    第一次上传文件

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+   
+   double myDouble = 12.345678;
+   Console.WriteLine("{0,-10:G}",myDouble);
+   Console.WriteLine("{0,-10}",myDouble);
+   Console.WriteLine("{0,-10:f4}",myDouble);
+   Console.WriteLine("{0,-10:C}",myDouble);
+   Console.WriteLine("{0,-10:D4}",12);
+   Console.WriteLine("{0,-10:x}",1194719);
+   Console.WriteLine("{0:P2}",0.1221897); 
+    
